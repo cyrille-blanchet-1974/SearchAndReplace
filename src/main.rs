45d24374c3693dd243cff4af730d853aa57ed1a5
Cyrille Blanchet <cyrille.blanchet@gmail.com>
2020-09-14 22:43:31 +0200

    clippy fix

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use std::io::BufReader;
 pub fn replace(l:&str,searched:&str,replace:&str)->String{
     let debug = true;
     let original = String::from(l);
-    let col_pos = l.find(&searched).unwrap_or(l.len());
+    let col_pos = l.find(&searched).unwrap_or_else(|| l.len());
     //cut string in three parts
     let mut line = String::from(l);
     let after = line.split_off(col_pos + searched.len());
     let _s= line.split_off(col_pos);
     let before = line;
     //replace
-    let mut replaced= String::from(before);
+    let mut replaced= before;
     //replaced.push_str(before.as_str());
     replaced.push_str(replace);
     replaced.push_str(after.as_str());
@@ -36,33 +36,30 @@ pub fn read(fic: &str, searched_str : &str,replaced_str : &str, first_only:bool)
                 if let Ok(l) = line {
                     if l.contains(&searched_str)
                     {
-                        let res:String;
-                        if !already_find{
-                            res= replace(&l, searched_str, replaced_str);
+                        let _res:String= if !already_find || !first_only {
+                            replace(&l, searched_str, replaced_str)
                         }else{
-                            if !first_only{
-                                res= replace(&l, searched_str, replaced_str);
-                            }else{
-                                res = String::from(l);
-                            }
-                        } 
+                            l
+                        };
                         already_find = true;
                         //TODO : write res line
-                    }
-                    else
-                    {
+                    }else{
                         //TODO : write initial line
                     }
                 } //TODO : else ...
             }
         }
     }
-    false
+    already_find
 }
 
 fn main() {
     println!("Search and replace");
     if read("./src/main.rs","Search and replace","Found and replaced",true)
+    {
+        println!("found")
+    }
+    if read("./src/main.rs","Search and Replace","Found and replaced",true)
     {
         println!("found")
     }
